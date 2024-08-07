d56e52350a9732a0f8221485f8ad95141ca1bd92
unknown <davidarthurforbes@gmail.com>
2024-08-07 19:41:00 -0400

    assignment complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,23 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener('click', function() {
+        paragraph.innerHTML = "Houston, we have lift off!"
+    });
+
+    missionAbort.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor = "red"
+    })
+
+    missionAbort.addEventListener('mouseout', function(event) {
+        event.target.style.backgroundColor = ""
+    })
     
+    missionAbort.addEventListener('click', function () {
+        if (window.confirm ("Are you sure you want to abort the mission?")) {
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home."
+        }
+    })
 }
 
 window.addEventListener("load", init);
